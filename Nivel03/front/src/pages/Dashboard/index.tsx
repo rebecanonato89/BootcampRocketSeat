68bae0dc406d4b3abe059bb81665779b79b485be
REBECANONATO <rebecanonato89@gmail.com>
2021-04-28 19:44:03 -0300

    link para detalhes da página

--- Nivel03/front/src/pages/Dashboard/index.tsx
+++ Nivel03/front/src/pages/Dashboard/index.tsx
@@ -1,6 +1,7 @@
-import React, { useState, FormEvent } from 'react';
-import { Form, Repositories, Title } from './styles';
+import React, { useState, FormEvent, useEffect } from 'react';
+import { Form, Repositories, Error, Title } from './styles';
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 import logoImg from '../../assets/logo.svg';
 import api from '../../services/api';
 
@@ -15,17 +16,39 @@ interface Repository {
 
 const Dashboard: React.FC = () => {
     const [newRepo, setNewRepo] = useState("");
-    const [repositories, setRepositories] = useState<Repository[]>([]);
+    const [repositories, setRepositories] = useState<Repository[]>(() => {
+        const storageRepositories = localStorage.getItem('@GithubExplorer:repositories');
+
+        if (storageRepositories)
+            return JSON.parse(storageRepositories);
+        else
+            return [];
+    });
+    const [inputError, setInputError] = useState('');
+
+
+    useEffect(() => {
+        localStorage.setItem('@GithubExplorer:repositories', JSON.stringify(repositories));
+    }, [repositories]);
 
     async function handleAddRepopositories(event: FormEvent<HTMLFormElement>): Promise<void> {
         event.preventDefault();
 
-        const response = await api.get(`repos/${newRepo}`);
+        if (!newRepo) {
+            setInputError('Digite o autor/nome do repositório');
+            return;
+        }
 
-        const repository = response.data;
+        try {
+            const response = await api.get(`repos/${newRepo}`);
 
-        setRepositories([...repositories, repository]);
-        setNewRepo('');
+            const repository = response.data;
+
+            setRepositories([...repositories, repository]);
+            setNewRepo('');
+        } catch (err) {
+            setInputError('Erro na busca por esse repositório');
+        }
 
     }
 
@@ -35,7 +58,7 @@ const Dashboard: React.FC = () => {
             <img src={logoImg} alt="GitHub Explorer" />
             <Title>Explore repositórios no GitHub</Title>
 
-            <Form onSubmit={handleAddRepopositories}>
+            <Form hasError={!!inputError} onSubmit={handleAddRepopositories}>
                 <input
                     value={newRepo}
                     onChange={e => setNewRepo(e.target.value)}
@@ -43,9 +66,11 @@ const Dashboard: React.FC = () => {
                 <button type="submit">Pesquisar</button>
             </Form>
 
+            {inputError && <Error>{inputError}</Error>}
+
             <Repositories>
                 {repositories.map(repository => (
-                    <a key={repository.full_name} href="teste">
+                    <Link key={repository.full_name} to={`/repositories/${repository.full_name}`}>
                         <img
                             src={repository.owner.avatar_url}
                             alt={repository.owner.login}
@@ -56,7 +81,7 @@ const Dashboard: React.FC = () => {
                         </div>
 
                         <FiChevronRight size={20} />
-                    </a>
+                    </Link>
                 ))}
             </Repositories>
         </>
--- Nivel03/front/src/pages/Repository/index.tsx
+++ Nivel03/front/src/pages/Repository/index.tsx
@@ -1,7 +1,69 @@
 import React from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
+
+interface RepositoryParams {
+    repository: string;
+}
 
 const Repository: React.FC = () => {
-    return <div>Repository</div>;
+    const { params } = useRouteMatch<RepositoryParams>();
+
+
+    return (
+        <>
+            <Header>
+                <img src={logoImg} alt="GitHub Explorer" />
+                <Link to="/">
+                    <FiChevronLeft size={16} />
+                    Voltar
+                </Link>
+            </Header>
+
+            <RepositoryInfo>
+                <header>
+                    <img
+                        src="https://avatars0.githubusercontent.com/u/28929274?v=4"
+                        alt="Rocketseat"
+                    />
+                    <div>
+                        <strong>rocketseat/unform</strong>
+                        <p>Descrição do repositório</p>
+                    </div>
+                </header>
+                <ul>
+                    <li>
+                        <strong>1808</strong>
+                        <span>Stars</span>
+                    </li>
+                    <li>
+                        <strong>48</strong>
+                        <span>Forks</span>
+                    </li>
+                    <li>
+                        <strong>67</strong>
+                        <span>Issues abertas</span>
+                    </li>
+                </ul>
+            </RepositoryInfo>
+
+            <Issues>
+                <Link to="asdasd">
+                    <div>
+                        <strong>asdasdasd</strong>
+                        <p>asdasdasd</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </Link>
+            </Issues>
+        </>
+    );
+
 };
 
 export default Repository;
